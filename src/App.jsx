9b778ae65aba209cd5cfdccbfef04d83883597c2
kirtan1313 <mkirtan013@gmail.com>
2024-05-16 19:35:09 +0530

    react pr 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Card from "./componets/Card/Card";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <Card />
+      </div>
+    </div>
+  )
+}
+
+export default App;
